640548f1673147a534d47a6bfb15356ed245c3ac
safwanvk <safwanvalakundil@gmail.com>
2021-03-03 16:54:10 +0530

    :zap: Update Tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -7,3 +7,19 @@ class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+
+    
+    class Meta:
+        ordering = ['-id']
+    
+    
+    def serialize(self):
+        '''
+        Feel free to delete!
+        '''
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
+        }
