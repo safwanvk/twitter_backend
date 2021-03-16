62e8a2f7a7210cd069437264d9e7cddf2e6c4bc9
safwanvk <safwanvalakundil@gmail.com>
2021-03-16 16:37:53 +0530

    :zap: Set limit in list view by username

--- tweets/views.py
+++ tweets/views.py
@@ -16,11 +16,15 @@ def tweet_create_view(request, *args, **kwargs):
         return Response(serializer.data, status=201)
     return Response({}, status=400)
 
+
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
+    username = request.GET.get('username') # ?username=Justin
+    if username != None:
+        qs = qs.filter(user__username__iexact=username)
     serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data, status=200)
+    return Response( serializer.data, status=200)
 
 @api_view(['GET'])
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
