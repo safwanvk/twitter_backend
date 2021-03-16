d231efa551337c3165ef38e81ee078b0f8f7d0c7
safwanvk <safwanvalakundil@gmail.com>
2021-03-16 16:27:40 +0530

    :sparkes: Setup dev auth

--- twitter_backend/settings.py
+++ twitter_backend/settings.py
@@ -134,14 +134,18 @@ DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
     ]
 
+DEFAULT_AUTHENTICATION_CLASSES = [
+    'rest_framework.authentication.SessionAuthentication'
+]
 if DEBUG:
     DEFAULT_RENDERER_CLASSES += [
         'rest_framework.renderers.BrowsableAPIRenderer',
     ]
+    DEFAULT_AUTHENTICATION_CLASSES += [
+        'twitter_backend.rest_api.dev.DevAuthentication'
+    ]
 REST_FRAMEWORK = {
     
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication'
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': DEFAULT_AUTHENTICATION_CLASSES,
     'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
 }
