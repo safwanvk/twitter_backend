34872f077950c8851931e2312d8ce29ede8b651a
safwanvk <safwanvalakundil@gmail.com>
2021-03-16 19:44:28 +0530

    :sparkes: Setup login_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from rest_framework.response import Response
 
 # Create your views here.
+def login_view(request, *args, **kwargs):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return Response({'msg': 'Success'}, status=200)
+    
+    return Response({'msg': 'Invalid Username'}, status=400)
--- twitter_backend/urls.py
+++ twitter_backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tweets/', include('tweets.urls'))
+    path('api/tweets/', include('tweets.urls')),
+    path('api/', include('accounts.urls'))
 ]
