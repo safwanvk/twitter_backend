d377f05b27577f78788d5a041ddf1ae54a9aac0f
safwanvk <safwanvalakundil@gmail.com>
2021-03-16 19:51:53 +0530

    :sparkes: Setup register view

--- accounts/views.py
+++ accounts/views.py
@@ -19,3 +19,14 @@ def logout_view(request, *args, **kwargs):
         return Response({'msg': 'Success'}, status=200)
     
     return Response({'msg': 'Server Error'}, status=500)
+
+def register_view(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=True)
+        user.set_password(form.cleaned_data.get("password1"))
+        # send a confirmation email to verify their account
+        login(request, user)
+        return Response({'msg': 'Success'}, status=200)
+        
+    return Response({'msg': 'Server Error'}, status=500)
